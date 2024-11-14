d3e3c303fe8e6a21694b7edafaa72092b6b6bfa4
hatran216004 <qiqisokiu.321@gmail.com>
2024-11-15 00:19:36 +0700

    deploy #5

--- server.js
+++ server.js
@@ -25,18 +25,21 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log('App running on port 3000...');
-});
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => {
+//   console.log('App running on port 3000...');
+// });
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION 🥱, SHUTTING DOW...');
-  server.close(() => {
-    process.exit(1); // 0: stands for a success, 1: stands for uncaught exception
-  });
+  // server.close(() => {
+  //   process.exit(1); // 0: stands for a success, 1: stands for uncaught exception
+  // });
+  process.exit(1);
 });
+
+module.exports = app;
 
 /*
   - Architecture MVC: Controller layer and the funtion of the controllers is to handle the application request, 
